3d1ed733bffbc403f6170fe55e037a82d7d5690b
vigzmv <vigzmv@outlook.com>
2017-10-14 21:23:57 +0530

    Chrome-Ext. :[Changed] Pop-up UI renewed

--- chrome-extension/src/popup/components/App.js
+++ chrome-extension/src/popup/components/App.js
@@ -1,37 +1,42 @@
-import React from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import syntaxStyle from './syntaxStyle'
-import styles from './App.css'
+import React from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import syntaxStyle from './syntaxStyle';
+import styles from './App.css';
+
+import MdSave from 'react-icons/lib/md/save';
+
+import MdAutorenew from 'react-icons/lib/md/autorenew';
+import MdPlayCircleOutline from 'react-icons/lib/md/play-circle-outline';
 
 const App = props => (
   <div>
-    <SyntaxHighlighter language='javascript' style={syntaxStyle}>
+    <SyntaxHighlighter language="javascript" style={syntaxStyle}>
       {`const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
 nightmare
 ${props.recording.reduce((records, record, i) => {
-  const { action, url, selector, value } = record
-  let result = records
-  if (i !== records.length) result += '\n'
+        const { action, url, selector, value } = record;
+        let result = records;
+        if (i !== records.length) result += '\n';
 
-  switch (action) {
-    case 'change':
-      result += `.type('${selector}', '${value}')`
-      break
-    case 'click':
-      result += `.click('${selector}')`
-      break
-    case 'goto':
-      result += `.goto('${url}')`
-      break
-    case 'reload':
-      result += `.refresh()`
-      break
-  }
+        switch (action) {
+          case 'change':
+            result += `.type('${selector}', '${value}')`;
+            break;
+          case 'click':
+            result += `.click('${selector}')`;
+            break;
+          case 'goto':
+            result += `.goto('${url}')`;
+            break;
+          case 'reload':
+            result += '.refresh()';
+            break;
+        }
 
-  return result
-}, '')}
+        return result;
+      }, '')}
 .end()
 .then(function (result) {
   console.log(result)
@@ -40,11 +45,37 @@ ${props.recording.reduce((records, record, i) => {
   console.error('Error:', error);
 });`}
     </SyntaxHighlighter>
+    <div className={styles.button_wrapper}>
+      <button className={styles.button} onClick={props.handleRestart}>
+        <MdPlayCircleOutline />
+      </button>
 
-    <button className={styles.button} onClick={props.handleRestart}>Restart</button>
+      <button
+        className={styles.button}
+        onClick={() => {
+          const data = {
+            code: document
+              .querySelector('#root > div > pre > code')
+              .innerText.replace(/(\r\n|\n|\r)/gm, ''),
+          };
+          fetch('https://uhack-6f65a.firebaseio.com/auto_codes.json', {
+            method: 'post',
+            body: JSON.stringify(data),
+          })
+            .then(response => response.json())
+            .then(data => {});
+        }}
+      >
+        <MdSave />
+      </button>
+
+      <button className={styles.button}>
+        <MdAutorenew />
+      </button>
+    </div>
   </div>
-)
+);
 
-App.displayName = 'App'
+App.displayName = 'App';
 
-export default App
+export default App;
